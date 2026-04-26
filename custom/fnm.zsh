81b6e25f4d3a159dc1ac5ec664a7d986cd29ce78
Ivan Alejandro <ivanalejandro0@gmail.com>
2026-04-26 17:14:57 -0300

    Tweak comments for fnm

--- custom/fnm.zsh
+++ custom/fnm.zsh
@@ -1,7 +1,11 @@
+# https://github.com/Schniz/fnm
 if ! hash fnm 2>/dev/null; then
     # fnm not found
     return
 fi
 
-# https://github.com/Schniz/fnm
+# note: this already adds `fnm` to PATH
 eval "$(fnm env)"
+
+# # default install path
+# FNM_DIR="/home/ivan/.local/share/fnm"
