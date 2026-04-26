ecd8a83810b41b24faba9d412ab571c616a6cf51
Ivan Alejandro <ivanalejandro0@gmail.com>
2026-04-26 17:16:00 -0300

    Add bun to PATH, export relevant dir

--- custom/bun.zsh
+++ custom/bun.zsh
@@ -1,2 +1,13 @@
 # bun completions
 [ -s "/home/ivan/.bun/_bun" ] && source "/home/ivan/.bun/_bun"
+
+# bun
+BUN_INSTALL="$HOME/.bun"
+BUN_BIN="$BUN_INSTALL/bin"
+
+if [[ ! -d $BUN_BIN ]]; then
+  return
+fi
+
+pathadd $BUN_BIN
+export BUN_INSTALL
