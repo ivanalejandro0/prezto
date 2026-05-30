aa99b8d4c4d65123ec162039333c09329173a294
Ivan Alejandro <ivanalejandro0@gmail.com>
2026-05-30 12:19:39 -0300

    Update go downloader

--- custom/golang.zsh
+++ custom/golang.zsh
@@ -3,15 +3,26 @@
 
 BASE=$HOME/golang
 
-# uncomment this when needed
+# # uncomment this when needed
 # function download_and_install_or_upgrade_go() {
 #     mkdir -p $BASE
-#     cd $BASE
+#     cd $BASE || exit 1
 #
-#     go_version="1.25.5"
+#     go_version="1.26.3"
+#     # TODO: compare with current version, use
+#     # $ go version
+#     # go version go1.26.3 linux/amd64
+#
 #     # go_tarball="go${go_version}.darwin-amd64.tar.gz"
 #     go_tarball="go${go_version}.linux-amd64.tar.gz"
-#     curl --remote-name https://dl.google.com/go/${go_tarball}
+#
+#     # curl --remote-name https://dl.google.com/go/${go_tarball}
+#     curl --remote-name https://go.dev/dl/${go_tarball}
+#
+#     # TODO: verify checksum
+#     # see https://github.com/golang/go/issues/68282#issuecomment-2205939815
+#     # download sha256 from https://go.dev/dl/?mode=json
+#
 #     [[ -d go.old ]] && rm -fr go.old
 #     [[ -d go ]] && mv go go.old
 #     tar xzf ${go_tarball}
